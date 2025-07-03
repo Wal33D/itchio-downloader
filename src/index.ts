fecc6514cac6c844e964f9cbc72666dfbb219179
Waleed Judah <aquataze@yahoo.com>
2025-07-03 03:26:52 -0400

    feat(cli): add typed argv interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { DownloadGameParams, DownloadGameResponse, IItchRecord } from './itchDownloader/types';
 
 import { downloadGame } from './itchDownloader/downloadGame';
+import { CLIArgs } from './types/cli';
 
 // Export the functions from this index file, making them accessible to users of your package
-export { downloadGame, DownloadGameParams, DownloadGameResponse, IItchRecord };
+export { downloadGame, DownloadGameParams, DownloadGameResponse, IItchRecord, CLIArgs };
---
+++ src/types/cli.ts
@@ -0,0 +1,6 @@
+export interface CLIArgs {
+   url?: string;
+   name?: string;
+   author?: string;
+   downloadDir?: string;
+}
